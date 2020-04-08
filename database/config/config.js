080e72968ea97dd32b96baf009cc465240389af5
Thomas <thomas@craftacademy.se>
2020-04-08 12:19:39 +0200

    adds sequelize and generates Book model

---
+++ database/config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "development": {
+    "username": "api_user",
+    "password": "password",
+    "database": "books_development",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "api_user",
+    "password": "password",
+    "database": "books_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "books_production",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  }
+}
